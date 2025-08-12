2b047fab9a7d4396885b7b2ea69a9022f6d22833
Dennis Mei <dmei22@hotmail.com>
2025-08-12 23:27:02 +0200

    first commit

---
+++ src/test/java/nl/miwnn/ch16/dennis/Ngredients/NgredientsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch16.dennis.Ngredients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgredientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
