11c1a0cd4b4f9773208787936bb249240622ca12
Dennis Mei <dmei22@hotmail.com>
2025-08-12 23:28:59 +0200

    Added Recipe model

---
+++ src/main/java/nl/miwnn/ch16/dennis/Ngredients/models/Recipe.java
@@ -0,0 +1,63 @@
+package nl.miwnn.ch16.dennis.Ngredients.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+public class Recipe {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    private String description;
+    private String ingredients;
+    private String instructions;
+    private String preparationTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getPreparationTime() {
+        return preparationTime;
+    }
+
+    public void setPreparationTime(String preparationTime) {
+        this.preparationTime = preparationTime;
+    }
+}
