ce7a6767940ee5bc8162a7716c7119f5f37bb687
Dennis Mei <dmei22@hotmail.com>
2025-08-12 23:54:06 +0200

    Added Recipe service and RecipeNotFoundException

---
+++ src/main/java/nl/miwnn/ch16/dennis/Ngredients/Exceptions/RecipeNotFoundException.java
@@ -0,0 +1,8 @@
+package nl.miwnn.ch16.dennis.Ngredients.Exceptions;
+
+
+public class RecipeNotFoundException extends RuntimeException {
+    public RecipeNotFoundException(String message) {
+
+    }
+}
---
+++ src/main/java/nl/miwnn/ch16/dennis/Ngredients/services/RecipeService.java
@@ -0,0 +1,43 @@
+package nl.miwnn.ch16.dennis.Ngredients.services;
+
+import nl.miwnn.ch16.dennis.Ngredients.Exceptions.RecipeNotFoundException;
+import nl.miwnn.ch16.dennis.Ngredients.models.Recipe;
+import nl.miwnn.ch16.dennis.Ngredients.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipeRepository.findAll();
+    }
+
+    public Recipe addOrUpdateRecipe(Recipe recipe) {
+        return recipeRepository.save(recipe);
+    }
+
+    public void deleteRecipe(Long id) {
+        recipeRepository.deleteById(id);
+    }
+
+    public Recipe findById(Long id) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+        if (recipeOptional.isPresent()) {
+            return recipeOptional.get();
+        } else {
+            throw new RecipeNotFoundException("Recipe by id " + id + " was not found");
+        }
+    }
+
+}
