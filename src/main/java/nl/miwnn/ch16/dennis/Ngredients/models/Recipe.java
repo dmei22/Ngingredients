a42a8921b4ac78f02fcdf784831f640fd9618181
Dennis Mei <dmei22@hotmail.com>
2025-08-13 10:50:37 +0200

    Added example JSON data

--- src/main/java/nl/miwnn/ch16/dennis/Ngredients/models/Recipe.java
+++ src/main/java/nl/miwnn/ch16/dennis/Ngredients/models/Recipe.java
@@ -2,12 +2,13 @@ package nl.miwnn.ch16.dennis.Ngredients.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Recipe {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String description;
