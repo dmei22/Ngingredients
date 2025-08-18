4bdc91924bd8da6a27d493aff1a14bbdfae90ab9
Dennis Mei <dmei22@hotmail.com>
2025-08-18 09:48:28 +0200

    Added app ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Recipe} from './recipe';
+import {RecipeService} from './recipe.service';
+import {catchError, of, tap} from 'rxjs';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
+
   protected readonly title = signal('Ngredients');
+
+  public recipes : Recipe[] = [];
+
+
+  constructor(private recipeService : RecipeService) {
+  }
+
+  ngOnInit() {
+    this.getRecipes();
+  }
+
+  private getRecipes() {
+    this.recipeService.getRecipes()
+      .pipe(
+        tap((response: Recipe[]) => {
+          this.recipes = response;
+          console.log(response);
+        }),
+        catchError((error: HttpErrorResponse) => {
+          alert(error.message);
+          return of([]);
+        })
+      )
+      .subscribe();
+  }
+
 }
