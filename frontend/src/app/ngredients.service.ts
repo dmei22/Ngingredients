6ee78da0d313b6fa118624d3ee8e2e7d5674f45b
Dennis Mei <dmei22@hotmail.com>
2025-08-18 09:31:59 +0200

    Added service

--- frontend/src/app/ngredients.service.ts
+++ frontend/src/app/ngredients.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {Recipe} from './recipe';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RecipeService {
-  
+export class NgredientsService {
+
+  private apiServerUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(`${this.apiServerUrl}/recipe/all`);
+  }
+
+  public addRecipe(recipe : Recipe): Observable<Recipe> {
+    return this.http.post<Recipe>(`${this.apiServerUrl}/recipe/add`, recipe);
+  }
+
+  public updateRecipe(recipe : Recipe): Observable<Recipe> {
+    return this.http.put<Recipe>(`${this.apiServerUrl}/recipe/update`, recipe);
+  }
+
+  public deleteRecipe(recipeId : number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/recipe/delete/${recipeId}`);
+  }
+
 }
