040785e22be40b05dd597a41e1b353365a000279
Dennis Mei <dmei22@hotmail.com>
2025-08-19 13:09:51 +0200

    Applied functionality to addrecipe form

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,10 +4,14 @@ import {Recipe} from './recipe';
 import {AppService} from './app.service';
 import {catchError, of, tap} from 'rxjs';
 import {HttpErrorResponse} from '@angular/common/http';
+import {FormsModule, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [
+    FormsModule
+  ],
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
@@ -39,4 +43,20 @@ export class AppComponent implements OnInit{
       .subscribe();
   }
 
+  public onAddRecipe(addForm: NgForm) {
+    document.getElementById("close-add-recipe-modal")?.click();
+
+    this.recipeService.addRecipe(addForm.value)
+      .pipe(
+        tap((response: Recipe) => {
+          addForm.reset();
+          console.log(response);
+          this.getRecipes();
+        }),
+        catchError((error: HttpErrorResponse) => {
+          alert(error.message);
+          return of(null);
+        })
+      ).subscribe();
+  }
 }
