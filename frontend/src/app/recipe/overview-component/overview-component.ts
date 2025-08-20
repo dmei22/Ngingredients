658ac8f8bb4741ef28d08a8f8730e1b289dfa734
Dennis Mei <dmei22@hotmail.com>
2025-08-20 12:10:08 +0200

    Added overview and detail components

---
+++ frontend/src/app/recipe/overview-component/overview-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-overview-component',
+  imports: [],
+  templateUrl: './overview-component.html',
+  styleUrl: './overview-component.css'
+})
+export class OverviewComponent {
+
+}
